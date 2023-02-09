1dd47adec7dd167c277cec3614e875b63d09c3a7
Jose Enrique Mata <josellique@gmail.com>
2023-02-08 21:50:46 -0600

    Semana 2, Intro a CSS

---
+++ src/main/java/com/practica_2/demo/Ea08PracticaJoseMataApplication.java
@@ -0,0 +1,13 @@
+package com.practica_2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ea08PracticaJoseMataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ea08PracticaJoseMataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica_2/demo/Ea08PracticaJoseMataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica_2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ea08PracticaJoseMataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
